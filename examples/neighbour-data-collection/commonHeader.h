8056dd945e178ce398531b7571c01a3684db80d1
RajibTheKing <aporba.das@gmail.com>
2020-12-10 04:35:32 +0100

    start sending data when any node knows their immediate best node and Finally Parse whole route in master node

--- examples/neighbour-data-collection/commonHeader.h
+++ examples/neighbour-data-collection/commonHeader.h
@@ -3,13 +3,20 @@
 #define COMMON_HEADER
 
 #define MAX_EXPECTED_NODES_IN_NETWORK 1000
+#define MAX_EXPECTED_SIZE_OF_ACTUAL_DATA 50
+
+#define PACKET_TYPE_BEACON 1
+#define PACKET_TYPE_ADVERTISEMENT 2
+#define PACKET_TYPE_ACTUAL_DATA 3
 
 typedef struct {
+  int packet_type;
   int nodeID;
   int seq;
 } Beacon;
 
 typedef struct {
+  int packet_type;
   int nodeId;
   int seq;
   float strengthToMaster; 
--- examples/neighbour-data-collection/neighbour-data-collection.c
+++ examples/neighbour-data-collection/neighbour-data-collection.c
@@ -22,18 +22,21 @@ int sequence_number = 0;
 static Beacon beaconData;
 static Beacon received_beaconData;
 
+
 static Advertisement advertisementData;
 static Advertisement received_advertisementData;
 static int counter = 0;
 static float maxMasterStrength = -1;
 static int bestNode = -1;
+static linkaddr_t bestLink;
 static int maxSeqforStrength = -1;
 
 static int mainStrengthSeq = 1;
 
 
 static Advertisement history;
-
+static uint8_t actualData[MAX_EXPECTED_SIZE_OF_ACTUAL_DATA];
+static int actualDataLen;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(nullnet_example_process, "NullNet broadcast example");
@@ -45,14 +48,17 @@ AUTOSTART_PROCESSES(&nullnet_example_process);
 void input_callback(const void *data, uint16_t len,
   const linkaddr_t *src, const linkaddr_t *dest)
 {
-  if (len == sizeof(Beacon)){
+  int type;
+  memcpy(&type, data, sizeof(int));
+
+  if (type == PACKET_TYPE_BEACON){
     memcpy(&received_beaconData, data, len);
 
     printf("TheKing--> received: (seq, node) = (%d, %d)\n", received_beaconData.seq, received_beaconData.nodeID);
 
     onReceivedNewBeacon(received_beaconData);
   }
-  if (len == sizeof(Advertisement)){
+  if (type == PACKET_TYPE_ADVERTISEMENT){
     memcpy(&received_advertisementData, data, len);
     printf("received adv from %d, seq : %d, currentlyMyBestNode: %d\n",received_advertisementData.nodeId, received_advertisementData.seq, bestNode);
     printFLoat(received_advertisementData.strengthToMaster);
@@ -73,10 +79,13 @@ void input_callback(const void *data, uint16_t len,
     if(currentSeq >= maxSeqforStrength && maxMasterStrength < currentStrength){
       maxMasterStrength = currentStrength;
       bestNode = received_advertisementData.nodeId;
+      memcpy(&bestLink, src, sizeof(linkaddr_t));
+      
     }
 
     
     nullnet_buf = (uint8_t *)&advertisementData;
+    advertisementData.packet_type = PACKET_TYPE_ADVERTISEMENT;
     advertisementData.nodeId = node_id;
     advertisementData.strengthToMaster = maxMasterStrength;
     advertisementData.seq = maxSeqforStrength;
@@ -100,6 +109,57 @@ void input_callback(const void *data, uint16_t len,
     }
     
   }
+
+  if (type == PACKET_TYPE_ACTUAL_DATA) {
+
+    if(node_id == 1){
+
+      memcpy(actualData, data, len);
+      int pos = 0;
+      pos += sizeof(int);
+      int numberOfNodes;
+      memcpy(&numberOfNodes, actualData+pos, sizeof(int));
+      pos  += sizeof(int);
+      int i;
+
+      printf("Got Actual Data --> Route (Size: %d): ", numberOfNodes + 1);
+      for(i = 0; i<numberOfNodes; i++, pos += sizeof(int)){
+        int curNode;
+        memcpy(&curNode, actualData + pos, sizeof(int));
+        if(i == 0){
+          printf("%d", curNode);
+        }else{
+          printf(" --> %d", curNode);
+        }
+        
+      }
+      printf(" --> %d\n", node_id);
+
+
+    }else{
+      memcpy(actualData, data, len);
+      int pos = 0;
+      pos += sizeof(int);
+      int numberOfNodes;
+      memcpy(&numberOfNodes, data+pos, sizeof(int));
+      numberOfNodes++;
+      memcpy(actualData + pos, &numberOfNodes, sizeof(int));
+      pos += sizeof(int);
+      pos += ((numberOfNodes-1) * sizeof(int));
+
+      
+      memcpy(actualData + pos, &node_id, sizeof(int));
+      actualDataLen = len + sizeof(int);
+
+      nullnet_buf = actualData;
+      nullnet_len = actualDataLen;
+      NETSTACK_NETWORK.output(&bestLink);
+      printf("TheKing--> Forwarding actualData (node) = (%d) --> to (node) = (%d)\n", node_id, bestNode);
+
+      
+    }
+
+  }
   
 
 }
@@ -119,6 +179,7 @@ PROCESS_THREAD(nullnet_example_process, ev, data)
   while(1) {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
     
+    beaconData.packet_type = PACKET_TYPE_BEACON;
     beaconData.seq = ++sequence_number;
     beaconData.nodeID = node_id;
     printf("TheKing--> Sending (seq, node) = (%d, %d)\n", beaconData.seq, beaconData.nodeID);
@@ -133,7 +194,9 @@ PROCESS_THREAD(nullnet_example_process, ev, data)
       maxMasterStrength = 100.0;
       bestNode = 1;
       //now send Advertisement
+      //Starting by Master
       nullnet_buf = (uint8_t *)&advertisementData;
+      advertisementData.packet_type = PACKET_TYPE_ADVERTISEMENT;
       advertisementData.nodeId = node_id;
       advertisementData.strengthToMaster = 100.0;
       advertisementData.seq = mainStrengthSeq++;
@@ -141,6 +204,31 @@ PROCESS_THREAD(nullnet_example_process, ev, data)
       nullnet_len = sizeof(Advertisement);
       NETSTACK_NETWORK.output(NULL);
       printf("TheKing--> Sending advertisement (node) = (%d)\n", advertisementData.nodeId);
+    }
+    else if(node_id != 1 && counter % 5 == 0){
+      //this block for All Slaves
+      //Send Data if Any reference to Master Available
+      if(bestNode != -1){
+        //there is a reference found
+        //now it's possible to send data to Master
+        //4 byte 
+        int numberOfNodes = 1;
+        int myNodeID = node_id;
+        int packet_type = PACKET_TYPE_ACTUAL_DATA;
+        int pos = 0;
+        memcpy(actualData, &packet_type, sizeof(int));
+        pos += sizeof(int);
+        memcpy(actualData + pos, &numberOfNodes, sizeof(int));
+        pos += sizeof(int);
+        memcpy(actualData + pos, &myNodeID, sizeof(int));
+        actualDataLen = 3 * sizeof(int);
+
+        nullnet_buf = actualData;
+        nullnet_len = actualDataLen;
+        NETSTACK_NETWORK.output(&bestLink);
+        printf("TheKing--> Sending actualData (node) = (%d) --> to (node) = (%d)\n", myNodeID, bestNode);
+
+      }
     }
     etimer_reset(&periodic_timer);
   }
